21c5012d4d0254d604b0418efda515733e608e7f
Giuseppe Rizza <ilmoro86@hotmail.it>
2025-01-21 16:11:20 +0100

    [feat] Add Snack 7

--- snack-7/script.js
+++ snack-7/script.js
@@ -21,3 +21,10 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+
+const studentIdTwo = students.find((element) => {
+  return students.id === 2;
+});
+
+console.log(studentIdTwo);
